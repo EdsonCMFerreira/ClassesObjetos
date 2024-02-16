316e56e31896f4671f926d80031bf8e9f07b4709
Edson Carlos de Melo Ferreira <edsoncarlosdemeloferreira@gmail.com>
2024-02-16 12:28:33 -0300

    Subindo projeto ClassesObjetos

---
+++ ClassesObjetos/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ClassesObjetos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // tipo inteiro
+            Program prog = new Program();
+
+            House house1 = new House();
+            house1.Address = "Rua xyz, 123";
+            house1.Color = "verde";
+
+            BankAccount bankAccount = new BankAccount();
+            bankAccount.PersonName = "Juca bala";
+            bankAccount.Deposit(200);  // Correção: Chamando o método Deposit corretamente
+        }
+    }
+
+    public class House
+    {
+        public string Address;
+        public string Color;
+
+        public void TurnLightsOn()
+        {
+            Console.WriteLine("Luzes ligadas.");  // Correção: Removendo o parâmetro value
+        }
+
+        public void TurnLightsOff()
+        {
+            Console.WriteLine("Luzes desligadas.");
+        }
+    }
+
+    public class BankAccount
+    {
+        public string PersonName;
+        public decimal Balance;
+
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+
+        public void ShowDetails()
+        {
+            Console.WriteLine($"{PersonName} possui saldo de {Balance:C2}");
+        }
+    }
+}
